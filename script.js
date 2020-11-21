15d64ce52ebd6548ccb18fbb68f0d39495a17c6b
Torklin <politecalshaxid@gmail.com>
2020-11-21 21:49:00 +0300

    Step 2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?');
 const personalMovieDB = {
     count: numberOfFilms, 
@@ -6,11 +7,29 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('Один из последних фильмов'),
-c = +prompt('Ваша оценка?'),
-b = prompt('Один из последних фильмов'),
-d = +prompt('Ваша оценка?');
-personalMovieDB.movies[a] = c;
-personalMovieDB.movies[b] = d;
+
+for(let i = 0; i < 2; i++){
+    const a = prompt('Один из последних фильмов'),
+    c = +prompt('Ваша оценка?');
+    
+    if(a != null && c != null && a != '' && c != '' && a.length < 50) {
+        personalMovieDB.movies[a] = c;
+    } else {
+        console.log('error');
+        i--;
+    }
+    
+
+}
+if (personalMovieDB.count < 10) {
+    alert ('Просмотрено мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 31) { 
+    alert ('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    alert ('Вы киноман');
+} else {
+    alert ('Произошла ошибка');
+}
+
 
 console.log(personalMovieDB);
